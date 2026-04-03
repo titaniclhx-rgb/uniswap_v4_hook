30a81d81616f23cf3934c47470e06b48a0401022
unknown <GL001170@igofoex.me>
2026-04-03 15:57:01 +0800

    hook

--- WhitelistBuyHook.sol
+++ WhitelistBuyHook.sol
@@ -8,18 +8,24 @@ import {Hooks}           from "v4-core/src/libraries/Hooks.sol";
 import {PoolKey}         from "v4-core/src/types/PoolKey.sol";
 import {SwapParams}      from "v4-core/src/types/PoolOperation.sol";
 import {BeforeSwapDelta} from "v4-core/src/types/BeforeSwapDelta.sol";
-/// import {console} from "forge-std/console.sol";
+
+
+interface IMsgSender {
+    function msgSender() external view returns (address);
+}
 
 contract WhitelistBuyHook is BaseHook {
     address public owner;
-    bool public restrictToken = true;
+    bool public restrictToken1 = true;
+    bool public restrictToken0 = false;
     mapping(address => bool) public whitelist;
 
     constructor(IPoolManager _poolManager) BaseHook(_poolManager) {
         owner = 0x679328c67c83dDA3741D0222D6D4A9F4a3a36e85;
         whitelist[owner] = true;
     }
-    
+
+
 	function getHookPermissions() public pure override returns (Hooks.Permissions memory) {
         return Hooks.Permissions({
             beforeInitialize: false,
@@ -36,30 +42,45 @@ contract WhitelistBuyHook is BaseHook {
             afterSwapReturnDelta: false,
             afterAddLiquidityReturnDelta: false,
             afterRemoveLiquidityReturnDelta: false
-			
         });
     }
-	
+
 	function _beforeSwap(address sender,
-						 PoolKey calldata /*key*/, // 暂时未使用
+						 PoolKey calldata /*key*/, // not use
 						 SwapParams calldata params,
-						 bytes calldata /*hookData*/) // 暂时未使用
-		internal view override returns (bytes4, BeforeSwapDelta, uint24) {
-		/// console.log("beforeSwap triggered!");
-        if (restrictToken && params.zeroForOne) {
-            require(whitelist[sender], "Not whitelisted for buying Token1");
+						 bytes calldata /*hookData*/
+	) internal view override returns (bytes4, BeforeSwapDelta, uint24) {
+		
+		address swapper;
+        try IMsgSender(sender).msgSender() returns (address res) {
+            swapper = res;
+        } catch {
+            revert("Router does not implement msgSender()");
         }
+		
+        if (restrictToken1 && params.zeroForOne) {
+            require(whitelist[swapper], "Not whitelisted for buying Token1");
+        }
+		
+		if (restrictToken0 && !params.zeroForOne) {
+            require(whitelist[swapper], "Not whitelisted for buying Token0");
+        }
+		
 		return (BaseHook.beforeSwap.selector, BeforeSwapDelta.wrap(0), 0);
     }
-	
+
     function setWhitelist(address addr, bool allowed) external {
         require(msg.sender == owner, "!owner");
         whitelist[addr] = allowed;
     }
 
-    function setRestrictToken(bool _restrictToken) external {
+    function setrestrictToken1(bool _restrictToken1) external {
         require(msg.sender == owner, "!owner");
-        restrictToken = _restrictToken;
+        restrictToken1 = _restrictToken1;
     }
 
+	function setrestrictToken0(bool _restrictToken0) external {
+        require(msg.sender == owner, "!owner");
+        restrictToken0 = _restrictToken0;
+    }
 }
